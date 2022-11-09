e593f0f121a79d8249d44a837bb506c8b4401219
gauravKverma <gauravverma004@gmail.com>
2022-11-09 21:29:47 +0530

    server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import axios from "axios";
 import express, { Request, Response, Application } from "express";
+const axios = require("axios")
 const cors = require("cors");
 const app: Application = express();
 const jwt = require("jsonwebtoken");
